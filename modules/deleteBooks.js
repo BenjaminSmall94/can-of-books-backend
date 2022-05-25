10d9c2a66fd73c6d9d5a23430a30960e0e23c840
Dana Huffman <dana.huffman@yahoo.com>
2022-05-24 19:48:00 -0700

    Removed console.log

--- modules/deleteBooks.js
+++ modules/deleteBooks.js
@@ -4,7 +4,6 @@ const Book = require('../models/book');
 
 async function getBooks (req, res, next) {
   try {
-    console.log(req.params.id);
     await Book.findByIdAndDelete(req.params.id);
     res.status(204).send('book deleted');
   } catch (error) {
